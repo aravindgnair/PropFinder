021a9931117d44c3ab61f8bb32b49690bfada9ea
Aravind G <aravindgnair@outlook.com>
2025-08-27 23:39:03 +0530

    Updated properties controller

--- backend/src/PropFinder.API/Controllers/PropertiesController.cs
+++ backend/src/PropFinder.API/Controllers/PropertiesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PropFinder.Application.DTOs;
 using PropFinder.Application.Interfaces;
 
@@ -20,6 +19,7 @@ public class PropertiesController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<PropertyDetailDto>), 200)]
     public async Task<IActionResult> Get(string? type, decimal? minPrice, decimal? maxPrice)
     {
         var properties = await _propertyService.GetPropertiesAsync(null, null, null);
@@ -28,21 +28,26 @@ public class PropertiesController : ControllerBase
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(PropertyDetailDto), 200)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> GetById(Guid id)
     {
         var property = await _propertyService.GetPropertyByIdAsync(id);
+
+        if (property == null)
+        {
+            return NotFound("Property not found");
+        }
 
         return Ok(property);
     }
 
     [HttpPost]
+    [ProducesResponseType(typeof(PropertyDetailDto), 200)]
     public async Task<IActionResult> Create([FromBody] CreatePropertyDto property)
     {
         var newProperty = await _propertyService.CreatePropertyAsync(property);
-
-        if (newProperty == null)
-            return NotFound("Property not found.");
-
+        
         return Ok(newProperty);
     }
 }
